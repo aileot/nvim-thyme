3e14417f6ed90a05a92f83ddf3ba75424e2d5781
aileot <aileot.foobar@gmail.com>
2024-06-03 05:30:32 +0900

    feat(health): add integration report

--- fnl/thyme/health.fnl
+++ fnl/thyme/health.fnl
@@ -1,6 +1,7 @@
 (local fennel (require :fennel))
 
 (local {: get-main-config} (require :thyme.config))
+(local {: get-runtime-files} (require :thyme.wrapper.nvim))
 
 (local (report-start report-info report-ok report-warn report-error)
        (let [health vim.health]
@@ -11,6 +12,20 @@
              ;; (The support will be dropped w/o deprecation notice.)
              (values health.report_start health.report_info health.report_ok
                      health.report_warn health.report_error))))
+
+(fn report-integrations []
+  (report-start "Thyme Integrations")
+  (let [reporter (if (= nil vim.g.parinfer_loaded)
+                     report-warn
+                     report-ok)]
+    (reporter (.. "vim.g.parinfer_loaded = " (tostring vim.g.parinfer_loaded))))
+  (let [dependency-files [:parser/fennel.so]]
+    ;; Note: The files "parser-info/*.revision" should only belong to
+    ;; https://github.com/nvim-treesitter/nvim-treesitter.
+    (each [_ file (ipairs dependency-files)]
+      (case (get-runtime-files [file] false)
+        [path] (report-ok (: "%s is detected at %s." :format file path))
+        _ (report-warn (: "missing %s." :format file))))))
 
 (fn report-thyme-config []
   (report-start "Thyme .nvim-thyme.fnl")
@@ -31,6 +46,7 @@
   (report-info "WIP: The root path of pool: "))
 
 {:check (fn []
+          (report-integrations)
           (report-thyme-config)
           (report-fennel-paths)
           (report-thyme-disk-info))}
--- lua/thyme/health.lua
+++ lua/thyme/health.lua
@@ -1,6 +1,8 @@
 local fennel = require("fennel")
 local _local_1_ = require("thyme.config")
 local get_main_config = _local_1_["get-main-config"]
+local _local_2_ = require("thyme.wrapper.nvim")
+local get_runtime_files = _local_2_["get-runtime-files"]
 local report_start, report_info, report_ok, report_warn, report_error = nil, nil, nil, nil, nil
 do
   local health = vim.health
@@ -9,6 +11,30 @@ do
   else
     report_start, report_info, report_ok, report_warn, report_error = health.report_start, health.report_info, health.report_ok, health.report_warn, health.report_error
   end
+end
+local function report_integrations()
+  report_start("Thyme Integrations")
+  do
+    local reporter
+    if (nil == vim.g.parinfer_loaded) then
+      reporter = report_warn
+    else
+      reporter = report_ok
+    end
+    reporter(("vim.g.parinfer_loaded = " .. tostring(vim.g.parinfer_loaded)))
+  end
+  local dependency_files = {"parser/fennel.so"}
+  for _, file in ipairs(dependency_files) do
+    local _5_ = get_runtime_files({file}, false)
+    if ((_G.type(_5_) == "table") and (nil ~= _5_[1])) then
+      local path = _5_[1]
+      report_ok(("%s is detected at %s."):format(file, path))
+    else
+      local _0 = _5_
+      report_warn(("missing %s."):format(file))
+    end
+  end
+  return nil
 end
 local function report_thyme_config()
   report_start("Thyme .nvim-thyme.fnl")
@@ -26,9 +52,10 @@ local function report_thyme_disk_info()
   report_info("WIP: The root path of module-mapping: ")
   return report_info("WIP: The root path of pool: ")
 end
-local function _3_()
+local function _7_()
+  report_integrations()
   report_thyme_config()
   report_fennel_paths()
   return report_thyme_disk_info()
 end
-return {check = _3_}
+return {check = _7_}
