bd683656cca67da147e931c30c94c83b7c4d103f
aileot <aileot.foobar@gmail.com>
2024-06-02 23:52:59 +0900

    feat(health): report config

--- fnl/thyme/health.fnl
+++ fnl/thyme/health.fnl
@@ -1,5 +1,11 @@
 (local fennel (require :fennel))
 
+(local {: get-main-config} (require :thyme.config))
+
+(fn report-thyme-config []
+  (vim.health.start "Thyme current config on .nvim-thyme.fnl")
+  (vim.health.info (fennel.view (get-main-config))))
+
 (fn report-fennel-paths []
   (vim.health.start "Thyme fennel.{path,macro-path}")
   (vim.health.info (.. "fennel.path:\n- " ;
@@ -8,4 +14,5 @@
                        (fennel.macro-path:gsub ";" "\n- "))))
 
 {:check (fn []
+          (report-thyme-config)
           (report-fennel-paths))}
--- lua/thyme/health.lua
+++ lua/thyme/health.lua
@@ -1,10 +1,17 @@
 local fennel = require("fennel")
+local _local_1_ = require("thyme.config")
+local get_main_config = _local_1_["get-main-config"]
+local function report_thyme_config()
+  vim.health.start("Thyme current config on .nvim-thyme.fnl")
+  return vim.health.info(fennel.view(get_main_config()))
+end
 local function report_fennel_paths()
   vim.health.start("Thyme fennel.{path,macro-path}")
   vim.health.info(("fennel.path:\n- " .. (fennel.path):gsub(";", "\n- ")))
   return vim.health.info(("fennel.macro-path:\n- " .. (fennel["macro-path"]):gsub(";", "\n- ")))
 end
-local function _1_()
+local function _2_()
+  report_thyme_config()
   return report_fennel_paths()
 end
-return {check = _1_}
+return {check = _2_}
