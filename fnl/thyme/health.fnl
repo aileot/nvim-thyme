b3130cece72fb1e97ce08ea194f401b32b06fe3b
aileot <aileot.foobar@gmail.com>
2024-06-03 00:04:28 +0900

    refactor(health): extract vim.health methods

--- fnl/thyme/health.fnl
+++ fnl/thyme/health.fnl
@@ -2,16 +2,25 @@
 
 (local {: get-main-config} (require :thyme.config))
 
+(local (report-start report-info report-ok report-warn report-error)
+       (let [health vim.health]
+         (if health.start
+             (values health.start health.info health.ok health.warn
+                     health.error)
+             ;; For nvim <0.10.0
+             ;; (The support will be dropped w/o deprecation notice.)
+             (values health.report_start health.report_info health.report_ok
+                     health.report_warn health.report_error))))
+
 (fn report-thyme-config []
-  (vim.health.start "Thyme current config on .nvim-thyme.fnl")
-  (vim.health.info (fennel.view (get-main-config))))
+  (report-start "Thyme current config on .nvim-thyme.fnl")
+  (report-info (fennel.view (get-main-config))))
 
 (fn report-fennel-paths []
-  (vim.health.start "Thyme fennel.{path,macro-path}")
-  (vim.health.info (.. "fennel.path:\n- " ;
-                       (fennel.path:gsub ";" "\n- ")))
-  (vim.health.info (.. "fennel.macro-path:\n- "
-                       (fennel.macro-path:gsub ";" "\n- "))))
+  (report-start "Thyme fennel.{path,macro-path}")
+  (report-info (.. "fennel.path:\n- " ;
+                   (fennel.path:gsub ";" "\n- ")))
+  (report-info (.. "fennel.macro-path:\n- " (fennel.macro-path:gsub ";" "\n- "))))
 
 {:check (fn []
           (report-thyme-config)
--- lua/thyme/health.lua
+++ lua/thyme/health.lua
@@ -1,17 +1,26 @@
 local fennel = require("fennel")
 local _local_1_ = require("thyme.config")
 local get_main_config = _local_1_["get-main-config"]
+local report_start, report_info, report_ok, report_warn, report_error = nil, nil, nil, nil, nil
+do
+  local health = vim.health
+  if health.start then
+    report_start, report_info, report_ok, report_warn, report_error = health.start, health.info, health.ok, health.warn, health.error
+  else
+    report_start, report_info, report_ok, report_warn, report_error = health.report_start, health.report_info, health.report_ok, health.report_warn, health.report_error
+  end
+end
 local function report_thyme_config()
-  vim.health.start("Thyme current config on .nvim-thyme.fnl")
-  return vim.health.info(fennel.view(get_main_config()))
+  report_start("Thyme current config on .nvim-thyme.fnl")
+  return report_info(fennel.view(get_main_config()))
 end
 local function report_fennel_paths()
-  vim.health.start("Thyme fennel.{path,macro-path}")
-  vim.health.info(("fennel.path:\n- " .. (fennel.path):gsub(";", "\n- ")))
-  return vim.health.info(("fennel.macro-path:\n- " .. (fennel["macro-path"]):gsub(";", "\n- ")))
+  report_start("Thyme fennel.{path,macro-path}")
+  report_info(("fennel.path:\n- " .. (fennel.path):gsub(";", "\n- ")))
+  return report_info(("fennel.macro-path:\n- " .. (fennel["macro-path"]):gsub(";", "\n- ")))
 end
-local function _2_()
+local function _3_()
   report_thyme_config()
   return report_fennel_paths()
 end
-return {check = _2_}
+return {check = _3_}
