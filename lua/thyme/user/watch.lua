02e6703034a70efd65f6c628e3f1356095c295ec
aileot <aileot.foobar@gmail.com>
2024-06-04 06:44:58 +0900

    fix(watch): clear cache when config is written

--- lua/thyme/user/watch.lua
+++ lua/thyme/user/watch.lua
@@ -1,22 +1,31 @@
-local _local_1_ = require("thyme.user.check")
-local check_to_update_21 = _local_1_["check-to-update!"]
+local _local_1_ = require("thyme.const")
+local config_path = _local_1_["config-path"]
+local _local_2_ = require("thyme.compiler.cache")
+local clear_cache_21 = _local_2_["clear-cache!"]
+local _local_3_ = require("thyme.user.check")
+local check_to_update_21 = _local_3_["check-to-update!"]
 local _3fgroup = nil
 local function watch_to_update_21(_3fopts)
   local group
-  local function _2_(...)
+  local function _4_(...)
     return vim.api.nvim_create_augroup("ThymeWatch", {})
   end
-  group = (_3fgroup or _2_())
+  group = (_3fgroup or _4_())
   local opts = (_3fopts or {})
   local event = (opts.event or {"BufWritePost", "FileChangedShellPost"})
   local pattern = (opts.pattern or "*.fnl")
   local callback
-  local function _4_(_3_)
-    local fnl_path = _3_["match"]
-    check_to_update_21(fnl_path, opts)
+  local function _6_(_5_)
+    local fnl_path = _5_["match"]
+    local resolved_path = vim.fn.resolve(fnl_path)
+    if (config_path == resolved_path) then
+      clear_cache_21()
+    else
+      check_to_update_21(resolved_path, opts)
+    end
     return nil
   end
-  callback = _4_
+  callback = _6_
   _3fgroup = group
   return vim.api.nvim_create_autocmd(event, {group = group, pattern = pattern, callback = callback})
 end
