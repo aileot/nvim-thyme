d139e0cf44cb1dcc27d52fe94a997ecbf1759c9c
aileot <46470475+aileot@users.noreply.github.com>
2024-12-21 23:39:24 +0900

    fix(uri): reduce chance to encounter ENAMETOOLONG

--- lua/thyme/utils/uri.lua
+++ lua/thyme/utils/uri.lua
@@ -1,25 +1,27 @@
 local _local_1_ = require("bit")
 local tohex = _local_1_["tohex"]
+local Path = require("thyme.utils.path")
+local appname = (vim.env.NVIM_APPNAME or "nvim")
+local split_pattern = ("/" .. appname .. "/")
 local function encode_with_percent(char)
   return ("%" .. tohex(string.byte(char), 2))
 end
 local function uri_encode(uri)
   local percent_patterns = "[^A-Za-z0-9%-_.!~*'()]"
-  local _2_ = uri:gsub(percent_patterns, encode_with_percent)
-  if (nil ~= _2_) then
-    local encoded = _2_
-    return encoded
-  else
-    return nil
-  end
+  local _start, _end = uri:find(split_pattern, 1, true)
+  local prefix = uri:sub(1, (_end - 1))
+  local suffix = uri:sub((_end + 1))
+  local prefix_encoded = prefix:gsub(percent_patterns, encode_with_percent)
+  local suffix_encoded = suffix:gsub(percent_patterns, encode_with_percent)
+  return Path.join(prefix_encoded, suffix_encoded)
 end
 local function hex__3echar(hex)
   return string.char(tonumber(hex, 16))
 end
 local function uri_decode(uri)
-  local _4_ = uri:gsub("%%([a-fA-F0-9][a-fA-F0-9])", hex__3echar)
-  if (nil ~= _4_) then
-    local decoded = _4_
+  local _2_ = uri:gsub("%%([a-fA-F0-9][a-fA-F0-9])", hex__3echar)
+  if (nil ~= _2_) then
+    local decoded = _2_
     return decoded
   else
     return nil
