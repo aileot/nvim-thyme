3fd7af5d2807f8c1ac3529ea0b7efd4cd2f41d1f
aileot <46470475+aileot@users.noreply.github.com>
2025-05-10 10:27:39 +0900

    refactor(cache): reduce Messenger
    in favor of the wrappers to clear caches: command `:ThymeCacheClear` and
    the callback of the autocmd group `ThymeWatch`.

--- fnl/thyme/compiler/cache.fnl
+++ fnl/thyme/compiler/cache.fnl
@@ -5,8 +5,6 @@
 
 (local {: lua-cache-prefix} (require :thyme.const))
 (local {: hide-files-in-dir!} (require :thyme.utils.pool))
-(local Messenger (require :thyme.utils.messenger))
-(local CacheMessenger (Messenger.new "cache"))
 (local {: clear-module-map-files!} (require :thyme.module-map.unit))
 
 (fn determine-lua-path [module-name]
@@ -24,13 +22,10 @@
 @return boolean return `true` when all the lua caches are cleared; otherwise, return `false`."
   ;; NOTE: glob is unavailable in vim.fs.find.
   (case (vim.fs.find #(= :.lua ($:sub -4)) {:type :file :path lua-cache-prefix})
-    [nil] (let [msg (.. "no cache files detected at " lua-cache-prefix)]
-            (CacheMessenger:notify! msg)
-            false)
-    _ (let [msg (.. "clear all the cache under " lua-cache-prefix)]
+    [nil] false
+    _ (do
         (hide-files-in-dir! lua-cache-prefix)
         (clear-module-map-files!)
-        (CacheMessenger:notify! msg)
         true)))
 
 {: determine-lua-path : clear-cache!}
--- lua/thyme/compiler/cache.lua
+++ lua/thyme/compiler/cache.lua
@@ -3,8 +3,6 @@ local _local_1_ = require("thyme.const")
 local lua_cache_prefix = _local_1_["lua-cache-prefix"]
 local _local_2_ = require("thyme.utils.pool")
 local hide_files_in_dir_21 = _local_2_["hide-files-in-dir!"]
-local Messenger = require("thyme.utils.messenger")
-local CacheMessenger = Messenger.new("cache")
 local _local_3_ = require("thyme.module-map.unit")
 local clear_module_map_files_21 = _local_3_["clear-module-map-files!"]
 local function determine_lua_path(module_name)
@@ -18,15 +16,11 @@ local function clear_cache_21()
   end
   _4_ = vim.fs.find(_5_, {type = "file", path = lua_cache_prefix})
   if ((_G.type(_4_) == "table") and (_4_[1] == nil)) then
-    local msg = ("no cache files detected at " .. lua_cache_prefix)
-    CacheMessenger["notify!"](CacheMessenger, msg)
     return false
   else
     local _ = _4_
-    local msg = ("clear all the cache under " .. lua_cache_prefix)
     hide_files_in_dir_21(lua_cache_prefix)
     clear_module_map_files_21()
-    CacheMessenger["notify!"](CacheMessenger, msg)
     return true
   end
 end
