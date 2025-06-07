1c6d70a02ee632dc9c43c6119f023e96c022736d
aileot <46470475+aileot@users.noreply.github.com>
2025-06-07 15:32:14 +0900

    fix(keymap): correctly show `fennel.view` results
    inserting missing `fennel.view` arg.

--- lua/thyme/user/keymap.lua
+++ lua/thyme/user/keymap.lua
@@ -38,7 +38,7 @@ Keymap["generate-plug-keymaps!"] = function(self, method)
     if ("string" == type(val)) then
       text = val
     else
-      text = fennel.view()
+      text = fennel.view(val)
     end
     return print_fn(text, {lang = self._lang})
   end
