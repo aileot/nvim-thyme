6b620238653df614a77e1a16399f5a2b0029c626
aileot <46470475+aileot@users.noreply.github.com>
2025-06-09 05:42:47 +0900

    refactor(const): copy `example-config-path` definition

--- lua/thyme/const.lua
+++ lua/thyme/const.lua
@@ -5,4 +5,12 @@ local rtp = vim.api.nvim_get_option_value("rtp", {})
 local config_path = vim.fn.resolve(Path.join(stdpath_config, config_filename))
 local cache_prefix = assert((rtp:match("([^,]+/thyme/compile[^,]-),") or rtp:match("([^,]+/thyme/compile[^,]-)$")), ("&runtimepath must contains a unique path which literally includes `/thyme/compile`; got " .. vim.inspect(vim.opt.rtp:get())))
 local lua_cache_prefix = vim.fn.expand(Path.join(cache_prefix, "lua"))
-return {["debug?"] = ("1" == vim.env.THYME_DEBUG), ["stdpath-config"] = stdpath_config, ["lua-cache-prefix"] = lua_cache_prefix, ["config-filename"] = config_filename, ["config-path"] = config_path, ["state-prefix"] = Path.join(vim.fn.stdpath("state"), "thyme")}
+local example_config_path
+do
+  local this_dir = vim.fs.dirname(debug.getinfo(1, "S").source:sub(2))
+  local example_config_filename = (config_filename .. ".example")
+  local _let_1_ = vim.fs.find(example_config_filename, {upward = true}, "type", "file", "path", this_dir)
+  local example_config_path0 = _let_1_[1]
+  example_config_path = example_config_path0
+end
+return {["debug?"] = ("1" == vim.env.THYME_DEBUG), ["stdpath-config"] = stdpath_config, ["lua-cache-prefix"] = lua_cache_prefix, ["config-filename"] = config_filename, ["config-path"] = config_path, ["example-config-path"] = example_config_path, ["state-prefix"] = Path.join(vim.fn.stdpath("state"), "thyme")}
